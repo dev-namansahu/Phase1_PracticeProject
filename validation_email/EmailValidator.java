aa06fca6b952ecf0c65b5ea7a956e0ca50abe91b
Naman Sahu <120280018+dev-namansahu@users.noreply.github.com>
2023-12-26 16:55:54 +0530

    Add files via upload

---
+++ validation_email/EmailValidator.java
@@ -0,0 +1,33 @@
+package validation_email;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+    private static final String EMAIL_REGEX =
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an email address: ");
+        String emailToValidate = scanner.nextLine();
+
+        if (isValidEmail(emailToValidate)) {
+            System.out.println("The email is valid.");
+        } else {
+            System.out.println("The email is invalid.");
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
+
+
